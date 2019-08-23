aec221ab7c99039c74170c3b9ee386e311ffe00a
Rene Schmitt <rene.schmitt01@sap.com>
2019-08-23 10:23:10 +0200

    Implement actor resolver with basic tests

--- src/schema/actor.js
+++ src/schema/actor.js
@@ -1,19 +1,31 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
-// Construct a schema, using GraphQL schema language
+const directorsData = require("../data/directors");
+
 const typeDef = gql`
-  type Author {
+  type Actor {
     name: String
     birthday: Date
     country: String
+    " should directors not be part of the Movie? "
     directors: [Director]
   }
 `;
 
 const resolvers = {
-  
-}
+  Actor: {
+    directors(actor) {
+      // return directors for all movies the actor had a role
+      return directorsData.getDirectors()
+        .filter((director) => {
+          const moviesWorkedTogether = director.movies.filter((movie) => (actor.movies.includes(movie)));
+          return moviesWorkedTogether.length > 0;
+        });
+    }
+  }
+};
 
 module.exports = {
   typeDef,
-}
+  resolvers,
+};
---
+++ src/schema/actor.test.js
@@ -0,0 +1,27 @@
+const actor = require("./actor");
+const actorsData = require("../data/actors");
+const moviesData = require("../data/movies");
+const directorsData = require("../data/directors");
+
+describe("actor", () => {
+  describe("resolvers", () => {
+    describe("directors", () => {
+      const resolver = actor.resolvers.Actor.directors;
+      it("should get all directors", () => {
+        const directors = resolver({movies: moviesData.getMovies().map((movie) => movie.title)});
+        expect(directors.length).toEqual(directorsData.getDirectors().length);
+      });
+      
+      it("should get all directors of movies the actor played in", () => {
+        const testActor = actorsData.getActors()[0];
+        const directors = resolver(testActor);
+        expect(directors.length).toEqual(testActor.movies.length); // assuming all movies in the test data have one director
+      });
+      
+      it("should return an empty array in case no movie matches", () => {
+        const directors = resolver({movies: ["Test Movie 1234"]});
+        expect(directors.length).toEqual(0);
+      });
+    });
+  });
+});
