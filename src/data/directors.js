edc3464ed8550035efd5afbedc77dc3efd61ca5b
Rene Schmitt <rene.schmitt01@sap.com>
2019-08-24 21:15:00 +0200

    Refactor director data access to be class based

--- src/data/directors.js
+++ src/data/directors.js
@@ -1,24 +1,53 @@
-module.exports = {
-  getDirectors: function getDirectors() {
-    return [
-      {
-        name: "Ridley Scott",
-        birthday: new Date(1937, 11, 30),
-        country: "UK",
-        movies: ["Alien", "The Martian", "Blade Runner"],
-      },
-      {
-        name: "Christopher Nolan",
-        birthday: new Date(1970, 7, 30),
-        country: "UK",
-        movies: ["Inception", "Interstellar", "The Dark Knight"],
-      },
-      {
-        name: "Peter Jackson",
-        birthday: new Date(1961, 10, 31),
-        country: "New Zeeland",
-        movies: ["The Hobbit: An Unexpected Journey", "The Lord of the Rings: The Fellowship of the Ring", "District 9"],
-      },
-    ];
+function getDirectors() {
+  return [
+    {
+      name: "Ridley Scott",
+      birthday: new Date(1937, 11, 30),
+      country: "UK",
+      movies: ["Alien", "The Martian", "Blade Runner"],
+    },
+    {
+      name: "Christopher Nolan",
+      birthday: new Date(1970, 7, 30),
+      country: "UK",
+      movies: ["Inception", "Interstellar", "The Dark Knight"],
+    },
+    {
+      name: "Peter Jackson",
+      birthday: new Date(1961, 10, 31),
+      country: "New Zeeland",
+      movies: ["The Hobbit: An Unexpected Journey", "The Lord of the Rings: The Fellowship of the Ring", "District 9"],
+    },
+  ];
+}
+
+class DirectorsData {
+  constructor() {
+    this._directors = getDirectors();
   }
+  
+  getDirectors() {
+    // return copy to avaid side effects
+    return this._directors.map((director => Object.assign({}, director)));
+  }
+  getDirectorsByNames(names) {
+    return this._directors.filter((director) => names.includes(director.name))
+      .map((director => Object.assign({}, director)));
+  }
+  getDirector(name) {
+    const directorsMatch = this._directors.filter((director) => director.name === name);
+    if (directorsMatch.length !== 1) {
+      throw Error("director does not exist");
+    }
+    return Object.assign({}, directorsMatch[0]);
+  }
+  getDirectorsByMovie(title) {
+    const directorsMatch = this._directors.filter((director) => director.movies.includes(title))
+      .map((director => Object.assign({}, director)));
+    return directorsMatch;
+  }
+}
+
+module.exports = {
+  DirectorsData
 };
---
+++ src/data/directors.test.js
@@ -0,0 +1,61 @@
+const directorsDataClass = require("./directors");
+const moviesDataClass = require("./movies");
+
+describe("directors test", () => {
+  let directorData;
+  let moviesData;
+  beforeEach(() => {
+    directorData = new directorsDataClass.DirectorsData();
+    moviesData = new moviesDataClass.MovieData();
+  });
+  describe("getDirectors", () => {
+    it("should return all directors", () => {
+      const result = directorData.getDirectors();
+      expect(result.length).toEqual(3); // ToDo: do not use a hard coded value
+    });
+  });
+  
+  describe("getDirectorsByTitles", () => {
+    it("should return all directors", () => {
+      const directors = directorData.getDirectors();
+      const result = directorData.getDirectorsByNames(directors.map((director) => director.name));
+      expect(result.length).toEqual(directors.length);
+      for(let i = 0; i < directors.length; i++) {
+        expect(result[i].name).toEqual(directors[i].name);
+        expect(result[i].birthday).toEqual(directors[i].birthday);
+      }
+    });
+  });
+  
+  describe("getDirector", () => {
+    it("should return an existing director", () => {
+      const director = directorData.getDirectors()[0];
+      const result = directorData.getDirector(director.name);
+      expect(result.name).toEqual(director.name);
+      expect(result.birthday).toEqual(director.birthday);
+    });
+    
+    it("should throw an error if the director does not exist", () => {
+      try {
+        directorData.getDirector("not a valid director");
+        fail("should have thrown an error");
+      } catch(error) {
+        expect(error).toBeDefined();
+      }
+    });
+  });
+  
+  describe("getDirectorsByMovie", () => {
+    
+    it("should return all directors", () => {
+      const movie = moviesData.getMovies()[0];
+      const result = directorData.getDirectorsByMovie(movie.title);
+      expect(result.length).toEqual(1); // ToDo: do not use a hard coded value
+    });
+    
+    it("should return empty array if no movies match", () => {
+      const result = directorData.getDirectorsByMovie("not a valid movie");
+      expect(result.length).toEqual(0); // ToDo: do not use a hard coded value
+    });
+  });
+});
