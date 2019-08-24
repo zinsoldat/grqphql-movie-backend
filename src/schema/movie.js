8a24b5dd23dd8bcfae630b5c5e19a290ac9b6bc4
Rene Schmitt <rene.schmitt01@sap.com>
2019-08-24 09:03:22 +0200

    Add new field with auth directive

--- src/schema/movie.js
+++ src/schema/movie.js
@@ -6,12 +6,17 @@ const typeDef = gql`
     title: String
     year: Int
     rating: Float
+    scoutbase_rating: String @auth
     actors: [Actor]
   }
 `;
 
 const resolvers = {
   Movie: {
+    scoutbase_rating() {
+      // random string between 5.0-9.0
+      return Math.floor(Math.random() * (90 - 50) + 50)/10;
+    },
     actors(movie, args, context) {
       return context.model.actor.getActors()
         .filter((actor) => (actor.movies.includes(movie.title)));
